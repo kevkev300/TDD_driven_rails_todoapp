5e1128dd4e370adba68b4dc89365793e377c41fd
Kevin Liebholz <kevin@liebholz.eu>
2020-02-10 14:40:20 +0100

    FEAT: allow todo to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
-  resources :todos, only: [:new, :create]
+  resources :todos, only: [:new, :create] do
+    resources :completions, only: :create
+  end
+
   resources :session, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_102158) do
+ActiveRecord::Schema.define(version: 2020_02_10_133442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_102158) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
