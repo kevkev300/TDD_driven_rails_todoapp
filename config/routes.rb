12806823a366ec873816ddac079e9ef680b8b39c
Kevin Liebholz <kevin@liebholz.eu>
2020-02-10 16:19:16 +0100

    FEAT: allowing todos to be marked incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
   resources :todos, only: [:new, :create] do
-    resources :completions, only: :create
+    resources :completions, only: [:create, :destroy]
   end
 
   resources :session, only: [:new, :create]
